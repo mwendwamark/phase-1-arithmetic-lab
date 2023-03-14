158555a54c62c6412af676eb6656493095cbde06
mark mwendwa <mwendwamark310@gmail.com>
2023-03-14 12:04:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply =num1* num2;
+
+let random =56;
+Number.isInteger(true);
+let max =20;
+Math.max=(2,4,6,8,10,12,14,16,18,20);
+max;
+
+let num3=9;
+let num4=5;
+let mod=num3-num4;
+mod;
